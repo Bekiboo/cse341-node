bba5c62c2cbfdd8189e7a74df1bcc005c4330126
Bekiboo <julien.connault@gmail.com>
2022-01-14 18:40:47 +0300

    Week 2

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const http = require('http')
+const path = require('path')
 
-const routes = require('./routes')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const server = http.createServer(routes)
+const app = express()
 
-server.listen(3000)
+app.set('view engine', 'ejs')
+app.set('views', 'views') // optional, it's already the default
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+// Body parser sert à traiter les données entrante pour chaque middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+// express.static permet d'établir un path pour un public folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// the browser sends a request to find the favicon, and it messes up the app.
+// This middleware fixes it.
+app.use('/favicon.ico', (req, res, next) => {
+  res.end()
+})
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
+})
+
+app.listen(3000)
