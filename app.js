b5f646bcee87ea3947180022752e9a7aa65ec34e
Bekiboo <julien.connault@gmail.com>
2022-01-28 15:21:28 +0300

    deploying to heroku again

--- app.js
+++ app.js
@@ -1,56 +1,57 @@
-const path = require('path');
+const path = require('path')
+const PATH = process.env.PORT || 5000
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require('./controllers/error')
+const User = require('./models/user')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
   User.findById('61f2766733b7225e2d16d305')
-    .then(user => {
-      req.user = user;
-      next();
+    .then((user) => {
+      req.user = user
+      next()
     })
-    .catch(err => console.log(err));
-});
+    .catch((err) => console.log(err))
+})
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
 mongoose
   .connect(
     'mongodb+srv://Bekiboo:mb2P35PTPadsRce@cluster0.q83of.mongodb.net/shop?retryWrites=true&w=majority'
   )
-  .then(result => {
-    User.findOne().then(user => {
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
           name: 'Julien',
           email: 'julien@test.com',
           cart: {
-            items: []
-          }
-        });
-        user.save();
+            items: [],
+          },
+        })
+        user.save()
       }
-    });
-    app.listen(3000);
+    })
+    app.listen(PATH)
   })
-  .catch(err => {
-    console.log(err);
-  });
+  .catch((err) => {
+    console.log(err)
+  })
