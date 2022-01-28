ead80ef7204b850552341c34d5c31279368a526c
Bekiboo <julien.connault@gmail.com>
2022-01-28 09:36:04 +0300

    Week 4

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', 'views') // optional, it's already the default
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// Body parser sert à traiter les données entrante pour chaque middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-// express.static permet d'établir un path pour un public folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(express.static(path.join(__dirname, 'public')));
 
-// the browser sends a request to find the favicon, and it messes up the app.
-// This middleware fixes it.
-app.use('/favicon.ico', (req, res, next) => {
-  res.end()
-})
+app.use((req, res, next) => {
+  User.findById('61f2766733b7225e2d16d305')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-app.listen(3000)
+mongoose
+  .connect(
+    'mongodb+srv://Bekiboo:mb2P35PTPadsRce@cluster0.q83of.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Julien',
+          email: 'julien@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
