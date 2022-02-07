c761e0d07bd1de9ffda94ebeb6b1c45a4365211a
Bekiboo <julien.connault@gmail.com>
2022-02-07 18:20:44 +0300

    Added Modal

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,11 @@
+const crypto = require('crypto')
+
 const bcrypt = require('bcryptjs/dist/bcrypt')
 // const nodemailer = require('nodemailer')
 // const sendgridTransport = require('nodemail-sendgrid-transport')
 
 const User = require('../models/user')
+const user = require('../models/user')
 
 // const transporter = nodemailer.createTransport(sendgridTransport({
 //   auth: {
@@ -12,15 +15,20 @@ const User = require('../models/user')
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error')
-  if (message.length > 0) {
-    message = message[0]
-  } else {
-    message = null
-  }
+  message.length > 0 ? message = message[0] : message = null
+  
+  let modal = req.flash('modal')
+  modal.length > 0 ? modal = modal[0] : modal = null
+  
+  let userEmail = req.flash('userEmail')
+  userEmail.length > 0 ? userEmail = userEmail[0] : userEmail = null
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     errorMessage: message,
+    modalMessage: modal,
+    userEmail: userEmail,
   })
 }
 
@@ -92,6 +100,8 @@ exports.postSignup = (req, res, next) => {
           return user.save()
         })
         .then((result) => {
+          req.flash('modal', 'You successfully signed up!')
+          req.flash('userEmail', email)
           res.redirect('/login')
           // return transporter.sendMail({
           //   to: email,
@@ -99,8 +109,9 @@ exports.postSignup = (req, res, next) => {
           //   subject: 'Signup succeeded!',
           //   html: '<h1>You successfully signed up!</h1>'
           // })
-        }).catch(err => {
-          console.log(err);
+        })
+        .catch((err) => {
+          console.log(err)
         })
     })
 
@@ -113,5 +124,45 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err)
     res.redirect('/')
+  })
+}
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message,
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if(!user) {
+          req.flash('error', 'No account with that email found')
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3600000
+        return user.save()
+      })
+      .then(result => {
+        
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   })
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,42 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
+const backdrop = document.querySelector('.backdrop')
+const sideDrawer = document.querySelector('.mobile-nav')
+const menuToggle = document.querySelector('#side-menu-toggle')
 
 function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
+  backdrop.style.display = 'none'
+  sideDrawer.classList.remove('open')
 }
 
 function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
+  backdrop.style.display = 'block'
+  sideDrawer.classList.add('open')
 }
 
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
+backdrop.addEventListener('click', backdropClickHandler)
+menuToggle.addEventListener('click', menuToggleClickHandler)
+
+// -------------------- MODAL --------------------
+// Get the modal
+const modal = document.getElementById('myModal')
+
+if (modal !== null) {
+  // Get the <span> element that closes the modal
+  const span = document.getElementsByClassName('close')[0]
+
+  // When the user clicks on the button, open the modal
+  // btn.onclick = function () {
+  //   modal.style.display = 'block'
+  // }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = 'none'
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none'
+    }
+  }
+}
