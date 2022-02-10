da523c1f51ea9d1de53d6f05fbd996ddf89e8844
Bekiboo <julien.connault@gmail.com>
2022-02-10 15:51:43 +0300

    Week 6

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,10 @@ const crypto = require('crypto')
 const bcrypt = require('bcryptjs/dist/bcrypt')
 // const nodemailer = require('nodemailer')
 // const sendgridTransport = require('nodemail-sendgrid-transport')
+const { validationResult } = require('express-validator')
 
 const User = require('../models/user')
-const user = require('../models/user')
+const { ResultWithContext } = require('express-validator/src/chain')
 
 // const transporter = nodemailer.createTransport(sendgridTransport({
 //   auth: {
@@ -15,18 +16,22 @@ const user = require('../models/user')
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error')
-  message.length > 0 ? message = message[0] : message = null
-  
+  message.length > 0 ? (message = message[0]) : (message = null)
+
   let modal = req.flash('modal')
-  modal.length > 0 ? modal = modal[0] : modal = null
-  
+  modal.length > 0 ? (modal = modal[0]) : (modal = null)
   let userEmail = req.flash('userEmail')
-  userEmail.length > 0 ? userEmail = userEmail[0] : userEmail = null
+  userEmail.length > 0 ? (userEmail = userEmail[0]) : (userEmail = null)
 
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+    },
+    validationErrors: [],
     modalMessage: modal,
     userEmail: userEmail,
   })
@@ -34,26 +39,56 @@ exports.getLogin = (req, res, next) => {
 
 exports.getSignup = (req, res, next) => {
   let message = req.flash('error')
-  if (message.length > 0) {
-    message = message[0]
-  } else {
-    message = null
-  }
+  message.length > 0 ? (message = message[0]) : (message = null)
+
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationErrors: [],
   })
 }
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
+      modalMessage: undefined,
+      userEmail: undefined,
+    })
+  }
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
         req.flash('error', 'Invalid email or password')
-        return res.redirect('/login')
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password',
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [],
+          modalMessage: undefined,
+          userEmail: undefined,
+        })
       }
       bcrypt
         .compare(password, user.password)
@@ -66,57 +101,76 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/')
             })
           }
-          req.flash('error', 'Invalid email or password')
-          res.redirect('/login')
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password',
+            oldInput: {
+              email: email,
+              password: password,
+            },
+            validationErrors: [],
+            modalMessage: undefined,
+            userEmail: undefined,
+          })
         })
         .catch((err) => {
           console.log(err)
           res.redirect('/login')
         })
     })
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
 }
 
 exports.postSignup = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
-  const confirmPassword = req.body.confirmPassword
 
-  User.findOne({ email: email })
-    .then((userDuplicate) => {
-      if (userDuplicate) {
-        req.flash('error', 'E-mail exists already, please pick a different one')
-        return res.redirect('/signup')
-        // add error message
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          })
-          return user.save()
-        })
-        .then((result) => {
-          req.flash('modal', 'You successfully signed up!')
-          req.flash('userEmail', email)
-          res.redirect('/login')
-          // return transporter.sendMail({
-          //   to: email,
-          //   from: 'shop@node-complete.com',
-          //   subject: 'Signup succeeded!',
-          //   html: '<h1>You successfully signed up!</h1>'
-          // })
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log('postSignup error: ', errors.array())
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword,
+      },
+      validationErrors: errors.array(),
     })
+  }
 
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      })
+      return user.save()
+    })
+    .then((result) => {
+      req.flash('modal', 'You successfully signed up!')
+      req.flash('userEmail', email)
+      res.redirect('/login')
+      // return transporter.sendMail({
+      //   to: email,
+      //   from: 'shop@node-complete.com',
+      //   subject: 'Signup succeeded!',
+      //   html: '<h1>You successfully signed up!</h1>'
+      // })
+    })
     .catch((err) => {
-      console.log(err)
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
     })
 }
 
@@ -129,11 +183,8 @@ exports.postLogout = (req, res, next) => {
 
 exports.getReset = (req, res, next) => {
   let message = req.flash('error')
-  if (message.length > 0) {
-    message = message[0]
-  } else {
-    message = null
-  }
+  message.length > 0 ? (message = message[0]) : (message = null)
+
   res.render('auth/reset', {
     path: '/reset',
     pageTitle: 'Reset Password',
@@ -149,8 +200,8 @@ exports.postReset = (req, res, next) => {
     }
     const token = buffer.toString('hex')
     User.findOne({ email: req.body.email })
-      .then(user => {
-        if(!user) {
+      .then((user) => {
+        if (!user) {
           req.flash('error', 'No account with that email found')
           return res.redirect('/reset')
         }
@@ -158,11 +209,77 @@ exports.postReset = (req, res, next) => {
         user.resetTokenExpiration = Date.now() + 3600000
         return user.save()
       })
-      .then(result => {
-        
+      .then((result) => {
+        req.flash('userEmail', req.body.email)
+        req.flash(
+          'modal',
+          `
+        You requested a password reset<br>
+        Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password
+        `
+        )
+        res.redirect('/')
       })
       .catch((err) => {
-        console.log(err)
+        const error = new Error(err)
+        error.httpStatusCode = 500
+        return next(error)
       })
   })
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() }, //$gt = greater than
+  })
+    .then((user) => {
+      let message = req.flash('error')
+      message.length > 0 ? (message = message[0]) : (message = null)
+
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      })
+    })
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password
+  const userId = req.body.userId
+  const passwordToken = req.body.passwordToken
+  let resetUser
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword
+      resetUser.resetToken = undefined
+      resetUser.resetTokenExpiration = undefined
+      return resetUser.save()
+    })
+    .then((result) => {
+      res.redirect('/login')
+    })
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
 }
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,119 +1,153 @@
-const Product = require('../models/product');
-const Order = require('../models/order');
+const Product = require('../models/product')
+const Order = require('../models/order')
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .then(products => {
-      console.log(products);
+    .then((products) => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
-      });
+      })
     })
-    .catch(err => {
-      console.log(err);
-    });
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
+  const prodId = req.params.productId
   Product.findById(prodId)
-    .then(product => {
+    .then((product) => {
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
         path: '/products',
-      });
+      })
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
 
 exports.getIndex = (req, res, next) => {
+  let message = req.flash('error')
+  message.length > 0 ? (message = message[0]) : (message = null)
+
+  let modal = req.flash('modal')
+  modal.length > 0 ? (modal = modal[0]) : (modal = null)
+
+  let userEmail = req.flash('userEmail')
+  userEmail.length > 0 ? (userEmail = userEmail[0]) : (userEmail = null)
   Product.find()
-    .then(products => {
+    .then((products) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-      });
+        errorMessage: message,
+        modalMessage: modal,
+        userEmail: userEmail,
+      })
     })
-    .catch(err => {
-      console.log(err);
-    });
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
 
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
     // .execPopulate()
-    .then(user => {
-      const products = user.cart.items;
+    .then((user) => {
+      const products = user.cart.items
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
         products: products,
-      });
+      })
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
+  const prodId = req.body.productId
   Product.findById(prodId)
-    .then(product => {
-      return req.user.addToCart(product);
+    .then((product) => {
+      return req.user.addToCart(product)
     })
-    .then(result => {
-      console.log(result);
-      res.redirect('/cart');
-    });
-};
+    .then((result) => {
+      console.log(result)
+      res.redirect('/cart')
+    })
+}
 
 exports.postCartDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+  const prodId = req.body.productId
   req.user
     .removeFromCart(prodId)
-    .then(result => {
-      res.redirect('/cart');
+    .then((result) => {
+      res.redirect('/cart')
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
 
 exports.postOrder = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
     // .execPopulate()
-    .then(user => {
-      const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: { ...i.productId._doc } };
-      });
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
+      })
       const order = new Order({
         user: {
           email: req.user.email,
-          userId: req.user
+          userId: req.user,
         },
-        products: products
-      });
-      return order.save();
+        products: products,
+      })
+      return order.save()
     })
-    .then(result => {
-      return req.user.clearCart();
+    .then((result) => {
+      return req.user.clearCart()
     })
     .then(() => {
-      res.redirect('/orders');
+      res.redirect('/orders')
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
 
 exports.getOrders = (req, res, next) => {
   Order.find({ 'user.userId': req.user._id })
-    .then(orders => {
+    .then((orders) => {
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
         orders: orders,
-      });
+      })
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
+}
